2f782cb9f731f9050a79b16dbcba407e4f8b818c
PilotKun <keerthi.modi1910@gmail.com>
2025-08-25 23:41:24 +0530

    Day-26: List Comprehension and Dictionary & Nato Game

---
+++ day26/Ex-4.py
@@ -0,0 +1,5 @@
+sentence = "what is the airspeed velocity of an unladen swallow?"
+
+result = {word:len(word) for word in sentence.split()}
+
+print(result)
--- day26/main.py
+++ day26/main.py
@@ -1,29 +1,28 @@
-
 # * Lists and Dictionary Comprehensions
 
 # * Basic usage: new_list = [new_item for item in list]
 # * Examples:
-numbers = [1,2, 3, 4, 5]
-new_numbers = [n +1 for n in numbers]
+numbers = [1, 2, 3, 4, 5]
+new_numbers = [n + 1 for n in numbers]
 print(new_numbers)
 
 name = "Keerthi"
 letters_list = [letter for letter in name]
 print(letters_list)
 
-new_range = [x *2 for x in range(1,5)]
+new_range = [x * 2 for x in range(1, 5)]
 print(new_range)
 
 # * Conditional List Comprehensions: new_list = [new_item for item in list if test]
 # * Examples:
-names = ['pilot','baku', 'shiro', 'chopper', 'felix', 'miya', 'hiki']
+names = ["pilot", "baku", "shiro", "chopper", "felix", "miya", "hiki"]
 short_names = [name for name in names if len(name) < 5]
-long_names = [name.upper() for name in names if len(name) >= 5]  
+long_names = [name.upper() for name in names if len(name) >= 5]
 print(short_names)
 print(long_names)
 
-# * Dictionary Comprehensions: new_dict = {new_key: new_value for item in list} 
-# OR 
-# * new_dict = {new_key: new_value for (key, value) in dict.items()} 
+# *  Dictionary Comprehensions: new_dict = {new_key: new_value for item in list}
+# OR
+# * new_dict = {new_key: new_value for (key, value) in dict.items()}
 # AND
 # * Conditional Dictionary Comprehensions: new_dict = {new_key: new_value for item in list if test}
